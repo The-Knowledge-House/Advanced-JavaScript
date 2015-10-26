8fc7d97eb146041dd1a4a616cf9fcb984a4f09a3
JoeC718 <joe.carrano@colorofchange.org>
2015-10-26 16:25:41 -0400

    added class 6 slides and excersizes

---
+++ Excersizes/DOM Vs. Server (Class 6)/practice.js
@@ -0,0 +1,22 @@
+var someNums = [1, 12, 15, 18, 20, 23, 26, 10, 33, 134];
+var num;
+var oddOrEven;
+
+
+var isEven = function(number){
+	if (number%2 == 0){
+		return true 
+	} else {
+		return false
+	}; 
+};
+
+for (var i = 0, len = someNums.length; i<len; i++){
+	num =  someNums[i];
+	if (isEven(num) == true){
+		oddOrEven = "Even"; 
+	} else{
+		oddOrEven = "Odd";
+	};
+	console.log( num + ' is ' + oddOrEven);
+}
