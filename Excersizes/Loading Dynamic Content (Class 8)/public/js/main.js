0e19c997b3e8d482d3f60e1cc357f0de681c8542
giovonnirodriguez <giovonni.rodriguez.12@cityas.org>
2015-10-27 15:22:51 -0700

    first commit

--- Excersizes/Loading Dynamic Content (Class 8)/public/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(e){
-	$('#submit').click(function(){
-		var parameters = { search: $('#search').val() };
-		$.get( '/searching',parameters, function(data) {
-        $('#results').html(data);
-
-		});
-	});
-});
